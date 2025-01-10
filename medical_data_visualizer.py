8417ff58120f31a54bc7cff8765ae7c26ee2ed0d
mendawg <zimek2006@gmail.com>
2025-01-10 21:26:24 +0100

    Update medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,31 +4,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] = np.where((df['weight'] / ((df['height'] / 100) ** 2)) > 25, 1, 0)
 
 # 3
+df['cholesterol'] = np.where(df['cholesterol'] > 1, 1, 0)
+df['gluc'] = np.where(df['gluc'] > 1, 1, 0)
 
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(df, id_vars=['cardio'],
+                     value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'],
+                     var_name='Parameter', value_name='Value')
+    df_cat.rename(columns={'cardio': 'cardiovascular'}, inplace=True)
 
     # 6
-    df_cat = None
-    
+    df_cat.groupby(['cardiovascular', 'Parameter', 'Value']).size().reset_index(name='Count')
 
     # 7
-
-
-
+    t = sns.catplot(data=df_cat, x='Value', hue='cardiovascular', col='Parameter', kind='count', height=6, aspect=1)
     # 8
-    fig = None
-
+    fig = t.fig
 
     # 9
     fig.savefig('catplot.png')
@@ -38,23 +38,31 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
 
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) & (
+                    df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) & (df['weight'] <= df['weight'].quantile(0.975))]
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
-
-
+    mask = ~np.triu(np.ones_like(corr, dtype=bool))
     # 14
-    fig, ax = None
+    n = len(corr.columns)
+    plt.figure(figsize=(n,n))
 
     # 15
+    sns.heatmap(
+        corr,
+        mask=mask,
+        annot=True,
+        fmt=".2f",
+        cmap="coolwarm",
+    )
+
+    plt.savefig('heatmap.png')
+    return 0
 
 
-
-    # 16
-    fig.savefig('heatmap.png')
-    return fig
+draw_heat_map()
